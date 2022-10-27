76a16ab988942680b6469e39796fd0187e25f53a
Sarah Jeong <jungej0106@gmail.com>
2022-10-27 16:33:50 -0600

    5.List Rendering

--- main.js
+++ main.js
@@ -3,8 +3,15 @@ const app = Vue.createApp({
       return {
           product: 'Socks',
           image: './assets/images/socks_blue.jpg',
-          onSale: false,
-          inventory: 8
+          inStock: true,
+          details: ['50% cotton', '30% wool', '20% polyester'],
+          // solution
+          sizes: ['S', 'M', 'L', 'XL'],
+          // solution
+          variants: [
+            { id: 2234, color: 'green' },
+            { id: 2235, color: 'blue' },
+          ]
       }
   }
 })
